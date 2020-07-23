e66518a50178a6c6aebe8a8ecc8250a0b4dbcfae
mirzaev.r.a <mirzaev.r.a@gmail.com>
2020-07-23 13:07:06 +0300

    Созадние страниц логина 8.

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {User} from "../../shared/interfaces";
 
 @Component({
   selector: 'app-login-page',
@@ -8,9 +9,10 @@ import {FormControl, FormGroup, Validators} from "@angular/forms";
 })
 export class LoginPageComponent implements OnInit {
 
-  form:FormGroup
+  form: FormGroup
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
     this.form = new FormGroup({
@@ -18,7 +20,7 @@ export class LoginPageComponent implements OnInit {
         Validators.required,
         Validators.email
       ]),
-      password: new FormControl(null,[
+      password: new FormControl(null, [
         Validators.required,
         Validators.minLength(6)
       ]),
@@ -29,6 +31,11 @@ export class LoginPageComponent implements OnInit {
     if (this.form.invalid) {
       return
     }
+    const user: User = {
+      email: this.form.value.email,
+      password: this.form.value.password
+    }
 
+    console.log(user)
   }
 }
