305ce2e2db6bca0a7bf2d9e0b1f676512d193cae
mirzaev.r.a <mirzaev.r.a@gmail.com>
2020-07-23 16:25:17 +0300

    Созадние страниц логина 13. Настройка Guards

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -10,6 +10,7 @@ import {CreatePageComponent} from './create-page/create-page.component';
 import {EditPageComponent} from './edit-page/edit-page.component';
 import {SharedModule} from "../shared/shared.module";
 import {AuthService} from "./shared/services/auth.service";
+import {AuthGuard} from "./shared/services/auth.guard";
 
 @NgModule({
   declarations: [AdminLayoutComponent, LoginPageComponent, DashboardPageComponent, CreatePageComponent, EditPageComponent],
@@ -23,15 +24,15 @@ import {AuthService} from "./shared/services/auth.service";
         path: '', component: AdminLayoutComponent, children: [
           {path: '', redirectTo: '/admin/login', pathMatch: 'full'},
           {path: 'login', component: LoginPageComponent},
-          {path: 'dashboard', component: DashboardPageComponent},
-          {path: 'create', component: CreatePageComponent},
-          {path: 'post/:id/edit', component: EditPageComponent},
+          {path: 'dashboard', component: DashboardPageComponent, canActivate: [AuthGuard]},
+          {path: 'create', component: CreatePageComponent, canActivate: [AuthGuard]},
+          {path: 'post/:id/edit', component: EditPageComponent, canActivate: [AuthGuard]},
         ]
       }
     ])
   ],
   exports: [RouterModule],
-  providers: [AuthService]
+  providers: [AuthService, AuthGuard]
 })
 
 export class AdminModule {
