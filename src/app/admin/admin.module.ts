d959f74504baad29be3d21e5cad9cf7dc924a815
mirzaev.r.a <mirzaev.r.a@gmail.com>
2020-07-23 14:12:54 +0300

    Созадние страниц логина 10. Настройка firebase

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -8,8 +8,8 @@ import {LoginPageComponent} from './login-page/login-page.component';
 import {DashboardPageComponent} from './dashboard-page/dashboard-page.component';
 import {CreatePageComponent} from './create-page/create-page.component';
 import {EditPageComponent} from './edit-page/edit-page.component';
-import {AuthService} from "./shared/services/auth.service";
 import {SharedModule} from "../shared/shared.module";
+import {AuthService} from "./shared/services/auth.service";
 
 @NgModule({
   declarations: [AdminLayoutComponent, LoginPageComponent, DashboardPageComponent, CreatePageComponent, EditPageComponent],
