bc18dfd0670c483a9cad032f2b99d7b80d8b39ab
mirzaev.r.a <mirzaev.r.a@gmail.com>
2020-07-23 22:33:41 +0300

    Созадние страниц логина 16. Создание постов. Плагин для постов

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ import {Environment} from "./interface";
 
 export const environment: Environment = {
   production: true,
-  apiKey: 'AIzaSyAktbczVHHQBGTtPKlgAMSF5SNkbYlxbpA'
+  apiKey: 'AIzaSyAktbczVHHQBGTtPKlgAMSF5SNkbYlxbpA',
+  fbDbUrl: 'https://angular-blog-715ba.firebaseio.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,8 @@ import {Environment} from "./interface";
 
 export const environment: Environment = {
   production: false,
-  apiKey: 'AIzaSyAktbczVHHQBGTtPKlgAMSF5SNkbYlxbpA'
+  apiKey: 'AIzaSyAktbczVHHQBGTtPKlgAMSF5SNkbYlxbpA',
+  fbDbUrl: 'https://angular-blog-715ba.firebaseio.com'
 };
 
 /*
